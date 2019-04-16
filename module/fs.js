6fa7b9d87ebf06f334d8e3f7829667ed3a59da1c
张锋 <zhangfeng930101@qq.com>
2019-04-16 16:34:40 +0800

    fs文件系统

---
+++ module/fs.js
@@ -0,0 +1,53 @@
+// fs就是node内置的文件系统模块，负责读写文件
+
+// fs模块同时提供了异步和同步的方法
+
+'use strict'
+
+var fs = require('fs')
+
+// 异步读取文件
+fs.readFile('fs.txt', 'utf-8', function(err, data) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(1, bufferToString(data))
+  }
+})
+
+function bufferToString(bf) {
+  return bf.toString('utf-8')
+}
+
+function stringToBuffer(str) {
+  return Buffer.from(str, 'utf-8')
+}
+// 同步读取文件
+try {
+  var syncData = fs.readFileSync('fs1.txt')
+  console.log(2, syncData)
+} catch(err) {
+  // throw Error(err)
+  console.log(err.Error)
+}
+
+// 写文件
+var writeData = 'Hello, fs writeFile'
+fs.writeFile('fs-write.txt', writeData, function(err) {
+  if (err) {
+    console.log('写文件失败')
+  }
+})
+
+fs.writeFileSync('fs-writesync.txt', writeData)
+
+// 获取文件信息
+/**
+ * @param {String} path 文件路径
+ * @param {Function} callback 回调函数 参数是err和status 【fa.status】
+ */
+fs.stat('fs-writesync.txt', function(err, stats) {
+  console.log(stats)
+
+  fs.writeFileSync('fs-writesync.txt', JSON.stringify(stats))
+})
---
+++ module/global.js
@@ -0,0 +1,8 @@
+'use strict';
+console.log(global.process === process) // true
+console.log(process.version)
+console.log(process.platform)
+
+console.log(process.arch)
+console.log(process.pwd)
+console.log(process)
