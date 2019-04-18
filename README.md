f4cfa62c13fa67f53cdb6006dfc69cc7c076458f
张锋 <zhangfeng930101@qq.com>
2019-04-18 16:28:52 +0800

    url模块

--- README.md
+++ README.md
@@ -3,9 +3,13 @@
 # url模块的常见字段
 
 > ** protocol ** 协议
-* username 
-* password
-* 
+> ** username ** 用户名
+> ** password ** 
+> ** host ** 主机部分
+> 
+>> hostname 主机名
+>
+>> port 端口
 * protocol
 * protocol
 * protocol
--- module/url.js
+++ module/url.js
@@ -2,4 +2,15 @@
 
 var url = require('url')
 
-console.log(new url.URL('https://user:pass@sub.host.com:8080/p/a/t/h?query=string#hash'))
+const myUrl = new url.URL('https://user:pass@sub.host.com:8080/p/a/t/h?query=string#hash')
+
+console.log(myUrl.protocol, '协议')
+console.log(myUrl.username, '用户名')
+console.log(myUrl.password, '密码')
+console.log(myUrl.host, '主机部分')
+console.log('主机名'+ myUrl.hostname +', 端口:'+ myUrl.port)
+console.log(myUrl.pathname, '路径')
+console.log(myUrl.search, '序列化查询(query)部分')
+// myUrl.searchParams.getAll()
+console.log(myUrl.searchParams, '对query的读写')
+console.log(myUrl.hash, '分段hash部分')
