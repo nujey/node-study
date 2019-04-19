751bfe7e5c505442e4fe8f580dcc51a8a0ef4c8e
张锋 <zhangfeng930101@qq.com>
2019-04-19 09:23:21 +0800

    http-fs-url-path

--- module/http.js
+++ module/http.js
@@ -5,16 +5,35 @@ var http = require('http'),
     fs = require('fs')
 // const { URL } 
 // 解析当前目录:
-var workDir = path.resolve('.')
-console.log(new url.URL('https://user:pass@sub.host.com:8080/p/a/t/h?query=string#hash'))
+var workDir = path.resolve('.') // 当前目录
+
+// var filePath = path.join(workDir, 'module', 'es.html') // 在目录后面添加文件
+
+// 从命令行参数获取root目录，默认是当前的目录
+var root = path.resolve(process.argv[2] || '.')
 
 // 创建http server，并传入回调函数:
 var server = http.createServer(function(request, response) {
-  // console.log(request)
+  var pathname = url.parse(request.url).pathname
+  var currentFile = path.join(root, 'module', pathname)
+  // 获取文件状态
+  fs.stat(currentFile, function(err, stats) {
+    if (!err && stats.isFile) {
+      response.writeHead(200)
+      fs.createReadStream(currentFile).pipe(response)
+    } else if (err.errno == -2){
+      fs.createWriteStream(currentFile, '404测试')
+      // response.writeHead(200)
+      // response.end('404 Not Found')
+    } else {
+      response.writeHead(200)
+      response.end('404 Not Found')
+    }
+  })
   // 将HTTP响应200写入response, 同时设置Content-Type: text/html:
-  response.writeHead(200, { 'Content-Type': 'text/html' });
+  // response.writeHead(200, { 'Content-Type': 'text/html' });
   // 将HTTP响应的HTML内容写入response:
-  response.end('<h1>Hola,Bonos Tares!</h1>')
+  // response.end('<h1>Hola,Bonos Tares!</h1>')
 })
 
 // 服务器监听9090端口
